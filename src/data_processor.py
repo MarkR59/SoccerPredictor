c40fa988b2d519a8eba71d3be1f9d68ecc171919
Mark Rulli <mar664@scarletmail.rutgers.edu>
2024-06-13 17:40:23 -0400

    Added features for goals conceded and scored

--- src/data_processor.py
+++ src/data_processor.py
@@ -6,3 +6,10 @@ def process_data(df):
     df.columns = ['Date', 'HomeTeam', 'AwayTeam', 'HomeGoals', 'AwayGoals', 'FullTimeResult', 'HomeShotsOnTarget', 'AwayShotsOnTarget', 'HomeBooking', 'AwayBooking', 'HomeWinOdds', 'DrawOdds', 'AwayWinOdds']
     df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
     return df
+
+def add_match_outcomes(df):
+    """Add match outcome columns to the DataFrame."""
+    df['HomeWin'] = df['FullTimeResult'] == 'H'
+    df['AwayWin'] = df['FullTimeResult'] == 'A'
+    df['Draw'] = df['FullTimeResult'] == 'D'
+    return df
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,5 +1,7 @@
+import pandas as pd
+
 def calculate_team_form(df, team, num_matches=5):
-    team_matches = df[(df['HomeTeam'] == team) | (df['AwayTeam'] == team)]
+    team_matches = df[(df['HomeTeam'] == team) | (df['AwayTeam'] == team)] #get matches that team is playing in
     team_matches = team_matches.sort_values(by='Date', ascending=False).head(num_matches)
     
     team_matches['Points'] = team_matches.apply(
@@ -10,9 +12,43 @@ def calculate_team_form(df, team, num_matches=5):
     )
     return team_matches['Points'].sum()
 
-def add_match_outcomes(df):
-    """Add match outcome columns to the DataFrame."""
-    df['HomeWin'] = df['FullTimeResult'] == 'H'
-    df['AwayWin'] = df['FullTimeResult'] == 'A'
-    df['Draw'] = df['FullTimeResult'] == 'D'
+def calculate_avg_scored(df, team, num_matches=5):
+    team_matches = df[(df['HomeTeam'] == team) | (df['AwayTeam'] == team)] #get matches that team is playing in
+    team_matches = team_matches.sort_values(by='Date', ascending=False).head(num_matches)
+
+    goals_scored = 0
+    for index, row in team_matches.iterrows():
+        if row['HomeTeam'] == team:
+            goals_scored += row['HomeGoals']
+        else:
+            goals_scored += row['AwayGoals']
+    
+    avg_scored = goals_scored / num_matches
+    return avg_scored
+
+def calculate_avg_conceded(df, team, num_matches=5):
+    team_matches = df[(df['HomeTeam'] == team) | (df['AwayTeam'] == team)] #get matches that team is playing in
+    team_matches = team_matches.sort_values(by='Date', ascending=False).head(num_matches)
+
+    goals_conceded = 0
+    for index, row in team_matches.iterrows():
+        if row['HomeTeam'] == team:
+            goals_conceded += row['AwayGoals']
+        else:
+            goals_conceded += row['HomeGoals']
+    
+    avg_condeded = goals_conceded / num_matches
+    return avg_condeded
+
+def add_avg_scored(df, num_matches=5):
+    """Add average goals scored columns to the DataFrame."""
+    df['AvgHomeScored'] = df.apply(lambda row: calculate_avg_scored(df, row['HomeTeam'], num_matches), axis=1)
+    df['AvgAwayScored'] = df.apply(lambda row: calculate_avg_scored(df, row['AwayTeam'], num_matches), axis=1)
     return df
+
+def add_avg_condeded(df, num_matches=5):
+    """Add average goals scored columns to the DataFrame."""
+    df['AvgHomeConceded'] = df.apply(lambda row: calculate_avg_conceded(df, row['HomeTeam'], num_matches), axis=1)
+    df['AvgAwayConceded'] = df.apply(lambda row: calculate_avg_conceded(df, row['AwayTeam'], num_matches), axis=1)
+    return df
+
